967f517708f8416c5db0f7325749dcf8ce653d2d
cszczotka <cszczotka@interia.pl>
2023-09-10 18:38:03 +0200

    refactoring

---
+++ spark-app/src/main/java/com/ilab/spark/apps/JoinRandomStreamApp.java
@@ -0,0 +1,56 @@
+package com.ilab.spark.apps;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
+
+import java.util.UUID;
+
+import static com.ilab.spark.utils.SparkSessionUtils.createSparkSessionWithDelta;
+
+public class JoinRandomStreamApp {
+
+    private static String DATA_DIR = "data/" + UUID.randomUUID().toString();
+
+
+    public static void main(String[] args) {
+        try {
+            SparkSession session =  createSparkSessionWithDelta("JoinRandomStreamApp");
+
+            Dataset<Row> testStream = session.readStream().format("rate")
+                    .option("rowsPerSecond", "1").option("numPartitions", "1").load();
+
+            Dataset<Row> impressions = testStream
+                    .select(
+                            (functions.col("value").plus(15)).as("impressionAdId"),
+                            functions.col("timestamp").as("impressionTime"));
+
+            Dataset<Row> clicks = testStream
+                    .select(
+                            functions.col("value").as("clickAdId"),
+                            functions.col("timestamp").as("clickTime"));
+
+            Dataset<Row> impressionsWithWatermark =
+                    impressions.withWatermark("impressionTime", "20 seconds");
+            Dataset<Row> clicksWithWatermark =
+                    clicks.withWatermark("clickTime", "30 seconds");
+
+
+            Dataset<Row> result = impressionsWithWatermark.join(
+                    clicksWithWatermark,
+                    functions.expr(
+                            "clickAdId = impressionAdId AND " +
+                                    "clickTime >= impressionTime AND " +
+                                    "clickTime <= impressionTime + interval 1 hour"),
+                    "leftOuter"
+            );
+
+            result.writeStream().outputMode("append").format("console").option("checkpointLocation", DATA_DIR + "/output-checkpoint")
+                    .start().awaitTermination(60000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ spark-app/src/main/java/com/ilab/spark/apps/JoinStreamsApp.java
@@ -0,0 +1,79 @@
+package com.ilab.spark.apps;
+
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static com.ilab.spark.utils.SparkSessionUtils.createSparkSessionForStreaming;
+
+public class JoinStreamsApp {
+
+    private static String DATA_DIR = "data/" + UUID.randomUUID().toString();
+
+    private static void createDeltaTable(SparkSession spark) {
+        StructType mySchema = new StructType(new StructField[]{
+                new StructField("id", DataTypes.LongType, false, Metadata.empty()),
+                new StructField("aggregated_id", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("source", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("version", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("type", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("createDate", DataTypes.TimestampType, false, Metadata.empty())
+        });
+        long now = new Date().getTime();
+        List<Row> rows = Arrays.asList(
+                RowFactory.create(1000l, "xxx_1000", "xxx", 1, "type1", new Timestamp(now)),
+                RowFactory.create(1001l, "xxx_1001", "xxx", 1, "type1", new Timestamp(now)),
+                RowFactory.create(1001l, "xxx_1001", "xxx", 1, "type2", new Timestamp(now))
+        );
+
+        spark.createDataFrame(rows, mySchema).write().mode("overwrite").format("delta").save(DATA_DIR+"/input");
+
+    }
+    public static void main(String[] args) {
+        try {
+            SparkSession spark =  createSparkSessionForStreaming("JoinTwoStreamsApp");
+            createDeltaTable(spark);
+
+            Dataset<Row> ds1 = spark
+                    .readStream()
+                    .format("delta")
+                    .load(DATA_DIR + "/input")
+                    .filter((FilterFunction<Row>) r -> "type1".equalsIgnoreCase(r.getAs("type")))
+                    .withWatermark("createDate", "5 minute").alias("s1");;
+
+            Dataset<Row> ds2 = spark
+                    .readStream()
+                    .format("delta")
+                    .load(DATA_DIR + "/input")
+                    .filter((FilterFunction<Row>) r -> "type2".equalsIgnoreCase(r.getAs("type")))
+                    .withWatermark("createDate", "5 minute").alias("s2");;
+
+            //inner join
+            //Dataset<Row>  result = ds1.join(ds2, "id");
+            //left outer
+            Dataset<Row>  result = ds1.join(ds2, functions.expr(" s1.id = s2.id " +
+                    " and s1.createDate between s2.createDate and s2.createDate + interval 5 minute  "), "leftOuter");
+
+            /*
+            StreamingQuery query = result.select("s1.id", "s1.type").writeStream().outputMode("append").format("console").option("truncate", false).start();
+            query.awaitTermination();
+            */
+
+            result.select("s2.id", "s2.type").writeStream().outputMode("append").format("delta")
+                    .start(DATA_DIR+"/output").awaitTermination(60000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
